61e71b618331aab2ea34d909f1e28b51be1ded56
Attar Ansari <attaransari29@gmail.com>
2026-04-19 16:49:20 +0530

    Fixed backend crash and updated deployment config

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,31 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 
+// ROUTES IMPORT
+import authRoutes from "./routes/authRoutes.js";
+import transactionRoutes from "./routes/transactionRoutes.js";
+
 dotenv.config();
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
+// DB connect
 connectDB();
 
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/transactions", transactionRoutes);
+
+// test route
 app.get("/", (req, res) => {
   res.send("Smart Ledger API Running 🚀");
 });
 
+// PORT
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
