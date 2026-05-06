9fecd1f7f9a667563a7030b0d8a08075c193db90
Attar Ansari <attaransari29@gmail.com>
2026-05-06 18:35:05 +0530

    fixed login + transaction issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smart-ledger-2r5j.onrender.com/api", // 🔥 TEMP HARD CODE
+  baseURL: "http://localhost:5000/api", // 🔥 TEMP HARD CODE
 });
 
 export default API;
