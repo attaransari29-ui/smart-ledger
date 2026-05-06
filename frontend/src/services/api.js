b4b012935b5646cc611077182e3f4b488ec43347
Attar Ansari <attaransari29@gmail.com>
2026-05-06 17:13:36 +0530

    fixed login + transaction issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,16 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
-
-// 🔥 TOKEN AUTO ADD
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
+  baseURL: "https://smart-ledger-2r5j.onrender.com/api", // 🔥 TEMP HARD CODE
 });
 
 export default API;
